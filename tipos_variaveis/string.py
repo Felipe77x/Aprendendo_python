d406236988f4579f4d8cbd907ee01aa3382ea1ba
Felipex77 <dias20felipe@gmail.com>
2025-03-21 14:28:36 +0000

    aprendendo string

---
+++ tipos_variaveis/string.py
@@ -0,0 +1,11 @@
+"""
+    Nesta aula, vamos aprender sobre o tipo de variável texto em Python. 
+O Python é de tipagem dinâmica, o que significa que ele identifica o tipo de texto pela presença de aspas. 
+Existem várias formas de declarar uma variável de texto, a mais simples é usando aspas duplas ou simples. 
+Também é possível usar três aspas antes e depois do texto para permitir a quebra de linhas. 
+Outra opção é usar a barra invertida para quebrar o texto em duas linhas. 
+Recomenda-se usar aspas duplas por padrão. Além disso, vamos declarar algumas variáveis de texto, como nome completo, nome e sobrenome.
+"""
+
+# STRINGS
+
