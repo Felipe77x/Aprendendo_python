fe62fcd9090c7812971797cd5f8c391a885bc59a
Felipex77 <dias20felipe@gmail.com>
2025-03-25 13:04:46 +0000

    ok

--- tipos_variaveis/string.py
+++ tipos_variaveis/string.py
@@ -9,3 +9,53 @@ Recomenda-se usar aspas duplas por padrão. Além disso, vamos declarar algumas 
 
 # STRINGS
 
+# DECLARAÇÃO
+
+nome_completo = "Felipe"
+
+
+nome_completo_aspas = """
+Naruto
+
+"""
+
+nome_completo_quebra = "Felipe  \
+dias"
+
+# FORMATAÇÃO 
+
+sobrenome = "Dias"
+nome = "Dormir melhora a saude"
+
+print("Nome completo (1a forma):", nome_completo)
+print("Nome completo (2a forma):" + nome_completo)
+print("Nome completo (3a forma):" + "Gabriel" + "Casemiro")
+print("Nome completo (4a forma):" + "Gabriel", "Casemiro")
+print("Nome completo (5a forma):", nome_completo_aspas)
+print("Nome completo (6a forma):", nome_completo_quebra)
+print("Nome completo (7a forma): %s" % nome_completo)
+print("Nome completo (8a forma): %s %s" % (nome, sobrenome))
+print(f"Nome completo (9a forma): {nome} {sobrenome}") 
+print("Nome completo (10a forma): {} {}".format(sobrenome, nome))
+
+
+# Metodos de strings
+
+# Metodo upper
+
+fruta = "manga"
+
+
+
+print(fruta.upper())
+
+
+# Metodo Lower 
+
+animal = "GATO"
+
+
+
+print(animal.lower())
+
+
