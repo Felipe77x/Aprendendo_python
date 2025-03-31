257a6509ae9be4bae3305478be47d80fc8024206
Felipex77 <dias20felipe@gmail.com>
2025-03-31 14:54:36 +0000

    True e false

--- tipos_variaveis/booleanos.py
+++ tipos_variaveis/booleanos.py
@@ -6,4 +6,47 @@ Nas condicionais, temos blocos de código que são executados se uma determinada
 Podemos utilizar os operadores lógicos "and" e "or" para comparar duas entradas e obter uma saída. 
 O operador "and" só retorna verdadeiro se as duas condições forem verdade
 """
+# condição verdadeira 
+
+True
+
+#condição falsa
+
+False
+
+
+# se condição
+
+# se nao = else
+
+if True:
+    print("O bloco IF vai ser executado.")
+else:
+    print("Bloco ELSE não será executado")
+
+# Operadores lógicos: and e or
+ 
+ # AND  
+if True and True:
+   print("Bloco será executado")
+ 
+elif True and False: 
+   print("Bloco não será executado")
+ 
+elif False and False:
+   print("Bloco não será executado")
+else:
+   print("ok")
+   
+ # OR
+if True or False:
+   print("Bloco OR vai ser executado")
+ 
+elif False or False:
+   print("Bloco OR não vai ser executado")
+ 
+elif True or True:
+   print("Bloco OR vai ser executado")
+else: 
+   print("ok")
 
