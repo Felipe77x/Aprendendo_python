b4454f347ba4f523eb48ed92fe3ed4105d4628ae
Felipex77 <dias20felipe@gmail.com>
2025-04-03 15:06:01 +0000

    aprendendo lista

---
+++ tipos_variaveis/lista.py
@@ -0,0 +1,21 @@
+"""
+Nesta aula, vamos aprender sobre listas em Python. 
+Uma lista é uma coleção de elementos ordenáveis e mutáveis. 
+Podemos declarar uma lista usando colchetes e separando os elementos por vírgula. 
+Os elementos podem ser de diferentes tipos. 
+Podemos acessar os elementos da lista usando índices, que começam em 0. 
+Podemos exibir a lista completa ou apenas um elemento específico usando a função print. 
+Também podemos fatiar a lista, ou seja, obter um intervalo de elementos. 
+Podemos fatiar do começo até um elemento específico ou a partir de um elemento específico. 
+Na próxima aula, veremos sobre atribuição e os principais métodos das listas.
+"""
+
+# Declaração
+
+lista_carro = ["Ford ka",1, "Corolla",2, "Ferrari",3, True, False]
+
+# Exibindo lista
+
+print("Minha lista de carros:", lista_carro)
+
+# Exibindo lista
