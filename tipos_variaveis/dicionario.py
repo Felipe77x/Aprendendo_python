b5443c808b630ec23d136cc03842f102a9f02303
Felipex77 <dias20felipe@gmail.com>
2025-04-07 14:53:15 +0000

    dicionario

---
+++ tipos_variaveis/dicionario.py
@@ -0,0 +1,29 @@
+"""
+Nesta aula, vamos aprender sobre dicionários em Python. 
+Um dicionário é uma coleção não ordenada de pares chave-valor. 
+Podemos criar um dicionário usando chaves e armazenar informações dentro dele. 
+Cada chave é importante para acessar a informação posteriormente. 
+Podemos adicionar várias chaves e valores ao dicionário, inclusive outros dicionários. 
+Podemos exibir o dicionário usando a função print e acessar os valores usando as chaves. 
+Se tentarmos acessar uma chave que não existe, receberemos uma exceção KeyError. 
+Podemos adicionar novas chaves ao dicionário mesmo depois de criá-lo. 
+Na próxima aula, aprenderemos sobre os métodos para manipular dicionários.
+
+"""
+
+# criando um dicionario
+
+dicionario_pessoa = {"nome":  "Felipe", "idade": 20, "profissão": "Informatica"}
+
+# Exibindo dicionario
+
+print("Dicionario pessoa", dicionario_pessoa)
+
+# Acessando valores
+
+print("Nome:", dicionario_pessoa["nome"])
+
+dicionario_pessoa = ["Sobrenome"] = "Dias"
+
+
+
